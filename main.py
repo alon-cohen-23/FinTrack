574d193aa10c23e9bebca4fb6acde6d3336659af
alon-cohen-23 <aloncohen51@gmail.com>
2025-01-20 15:33:58 +0200

    Added README.md

--- main.py
+++ main.py
@@ -30,35 +30,35 @@ def main ():
     
     cities_df = pd.read_csv("tables/cities.csv")
     cities_df.to_sql("cities", engine, if_exists="append", index=False)
-    logger.info("Uploaded cities data")
+    logger.info("Uploaded cities table")
     
     zipcodes_df = pd.read_csv("tables/zipcodes.csv", dtype={"zipcode": str})
     zipcodes_df.to_sql("zipcodes", engine, if_exists="append", index=False)
-    logger.info("Uploaded zipcodes data")
+    logger.info("Uploaded zipcodes table")
     
     clients_data_df = pd.read_csv("tables/clients_data.csv", dtype={"zipcode": str})
     clients_data_df.to_sql("clients_data", engine, if_exists="append", index=False)
-    logger.info("Uploaded clients_data data")
+    logger.info("Uploaded clients_data table")
     
     cards_data_df = pd.read_csv("tables/cards_data.csv")
     cards_data_df.to_sql("cards_data", engine, if_exists="append", index=False)
-    logger.info("Uploaded cards_data data")
+    logger.info("Uploaded cards_data table")
     
     mcc_codes_df = pd.read_csv("tables/mcc_codes.csv")
     mcc_codes_df.to_sql("mcc_codes", engine, if_exists="append", index=False)
-    logger.info("Uploaded mcc_codes data")
+    logger.info("Uploaded mcc_codes table")
     
     merchants_data_df = pd.read_csv("tables/merchants_data.parquet", dtype={"zipcode": str})
     merchants_data_df.to_sql("merchants_data", engine, if_exists="append", index=False)
-    logger.info("Uploaded merchants_data data")
+    logger.info("Uploaded merchants_data table")
     
     transactions_df = pd.read_parquet("tables/transactions.parquet")
     transactions_df.to_sql("transactions", engine, if_exists="append", index=False)
-    logger.info("Uploaded transactions data")
+    logger.info("Uploaded transactions table")
     
     fraud_labels_df = pd.read_parquet("tables/fraud_labels.parquet")
     fraud_labels_df.to_sql("fraud_labels", engine, if_exists="append", index=False)
-    logger.info("Uploaded fraud_lables data")
+    logger.info("Uploaded fraud_lables table")
     
     logger.info("The pipline Ended, enjoy your new database")
     
